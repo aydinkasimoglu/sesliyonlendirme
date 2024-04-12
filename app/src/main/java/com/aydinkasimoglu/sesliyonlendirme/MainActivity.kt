dcf510461589d4cfcb28a370611a1497bcabfe6a
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2024-04-12 13:57:20 +0300

    Refactor MainActivity with composables

--- app/src/main/java/com/aydinkasimoglu/sesliyonlendirme/MainActivity.kt
+++ app/src/main/java/com/aydinkasimoglu/sesliyonlendirme/MainActivity.kt
@@ -1,64 +1,35 @@
 package com.aydinkasimoglu.sesliyonlendirme
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.Looper
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.core.app.ActivityCompat
 import com.aydinkasimoglu.sesliyonlendirme.ui.theme.SesliYonlendirmeTheme
-import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.Priority
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.HttpTimeout
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
-import io.ktor.client.statement.HttpResponse
-import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.json.Json
-
 
 class MainActivity : ComponentActivity() {
-    private lateinit var locationClient: FusedLocationProviderClient
-    private lateinit var locationCallback: LocationCallback
-    private val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+
+    private val voiceToText by lazy {
+        VoiceToText(application)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -85,7 +56,7 @@ class MainActivity : ComponentActivity() {
                     }
 
                     scope.launch(Dispatchers.IO) {
-                        directionsData = makeRequest(currentLocation)
+                        directionsData = getDirectionsData(currentLocation)
                     }
                 }
             }
@@ -96,129 +67,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DirectionsScreen(currentLocation = currentLocation, data = directionsData)
-                }
-            }
-        }
-    }
-
-    @OptIn(ExperimentalSerializationApi::class)
-    private suspend fun makeRequest(origin: Pair<Double, Double>): Step? {
-        val client = HttpClient(CIO) {
-            install(ContentNegotiation) {
-                json(Json {
-                    ignoreUnknownKeys = true
-                    explicitNulls = false
-                })
-            }
-
-            install(HttpTimeout) {
-                connectTimeoutMillis = HttpTimeout.INFINITE_TIMEOUT_MS
-            }
-        }
-
-        val response: HttpResponse = client.post("https://routes.googleapis.com/directions/v2:computeRoutes") {
-            url {
-                headers.append("Content-Type", "application/json")
-                headers.append("X-Goog-Api-Key", "AIzaSyAlsV41_jlFTFhzWwTLy7TpeioSuggP7Q0")
-                headers.append("X-Goog-FieldMask", "routes.legs")
-            }
-            setBody(RequestBody(
-                origin = Origin(RequestLocation(LatLng(origin.first, origin.second))),
-                destination = Destination(RequestLocation(LatLng(40.82408137774701, 29.920700725944666))),
-                travelMode = "WALK",
-                languageCode = "tr-TR",
-                units = "METRIC"
-            ))
-        }
-
-        client.close()
-
-        return if (response.status.value in 200..299) {
-            val data = response.body<Data>()
-            data.routes[0].legs[0].steps[0]
-        } else {
-            null
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun startLocationUpdates() {
-        locationCallback.let {
-            val locationRequest = LocationRequest.Builder(
-                Priority.PRIORITY_HIGH_ACCURACY,
-                100
-            )
-                .setWaitForAccurateLocation(false)
-                .setMinUpdateIntervalMillis(3000)
-                .setMaxUpdateDelayMillis(100)
-                .build()
-
-            locationClient.requestLocationUpdates(
-                locationRequest,
-                it,
-                Looper.getMainLooper()
-            )
-        }
-    }
-
-    @Composable
-    private fun DirectionsScreen(currentLocation: Pair<Double, Double>, data: Step?) {
-        val launcherMultiplePermissions =
-            rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestMultiplePermissions()) {
-                val areGranted = it.values.reduce { acc, next -> acc && next }
-
-                if (areGranted) {
-                    startLocationUpdates()
-                    Toast.makeText(this, "Permission granted", Toast.LENGTH_LONG).show()
-                }
-                else {
-                    Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show()
-                }
-        }
-
-        LaunchedEffect (key1 = permissions) {
-            if (permissions.all {
-                    ActivityCompat.checkSelfPermission(this@MainActivity, it) == PackageManager.PERMISSION_GRANTED
-                }) {
-                startLocationUpdates()
-            }
-            else {
-                launcherMultiplePermissions.launch(permissions)
-            }
-        }
-
-        Box (
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(20.dp)
-        ) {
-            Column (
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                ElevatedCard (
-                    elevation = CardDefaults.cardElevation(defaultElevation = 3.dp),
-                ) {
-                    Text (
-                        text =
-                            if (currentLocation == Pair(0.0, 0.0))
-                                "Konumunuz yükleniyor"
-                            else
-                                "Konumunuz: ${currentLocation.first}/${currentLocation.second}",
-                        modifier = Modifier.padding(horizontal = 6.dp, vertical = 12.dp),
-                    )
-                }
-                Column (
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    data?.also {
-                        it.navigationInstruction?.let { navigationInstruction ->
-                            Text(text = navigationInstruction.instructions)
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(20.dp)
+                    ) {
+                        Column {
+                            Speech(voiceToText = voiceToText)
+                            
+                            Spacer(modifier = Modifier.height(20.dp))
+                            
+                            DirectionsScreen(currentLocation = currentLocation, data = directionsData)
                         }
-                    } ?: CircularProgressIndicator (modifier = Modifier.width(45.dp))
+                    }
                 }
             }
         }
