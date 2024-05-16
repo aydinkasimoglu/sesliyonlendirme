5b510c1f9a7a3d4ab001d8cdb450a7f80ecfab78
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2024-05-16 12:43:29 +0300

    Changed animation of button

--- app/src/main/java/com/aydinkasimoglu/sesliyonlendirme/Speech.kt
+++ app/src/main/java/com/aydinkasimoglu/sesliyonlendirme/Speech.kt
@@ -10,32 +10,55 @@ import android.speech.SpeechRecognizer
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.EaseInOut
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import java.util.Locale
+import kotlinx.coroutines.launch
 
+/**
+ * State of the voice to text conversion
+ *
+ * @param spokenText The text that was spoken
+ * @param isSpeaking Whether the user is speaking
+ * @param error The error message if any
+ */
 data class VoiceToTextState(
     val spokenText: String = "",
     val isSpeaking: Boolean = false,
     val error: String? = null,
 )
 
+/**
+ * Recognizer for converting voice to text
+ *
+ * @param app The application
+ */
 class VoiceToText(private val app: Application): RecognitionListener {
     private val _state = MutableStateFlow(VoiceToTextState())
     val state = _state.asStateFlow()
@@ -107,6 +130,11 @@ class VoiceToText(private val app: Application): RecognitionListener {
     override fun onEvent(eventType: Int, params: Bundle?) = Unit
 }
 
+/**
+ * Composable for converting voice to text
+ *
+ * @param voiceToText The voice to text recognizer
+ */
 @Composable
 fun Speech(voiceToText: VoiceToText) {
     var canRecord by remember {
@@ -124,32 +152,47 @@ fun Speech(voiceToText: VoiceToText) {
 
     val state by voiceToText.state.collectAsState()
 
-    Column(
+    Row (
         modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
     ) {
-        Button(onClick = {
-            if (state.isSpeaking) {
-                voiceToText.stopListening()
-            } else {
-                voiceToText.startListening()
-            }
-        }) {
-            AnimatedContent(targetState = state.isSpeaking) { isSpeaking ->
-                if (isSpeaking) {
-                    Icon(painter = painterResource(id = R.drawable.round_stop_24), contentDescription = "Stop recording audio")
-                } else {
-                    Icon(painter = painterResource(id = R.drawable.round_mic_24), contentDescription = "Record audio")
-                }
-            }
+        val rotation = remember {
+            Animatable(0f)
         }
 
+        val painter = remember {
+            mutableIntStateOf(R.drawable.round_mic_24)
+        }
+
+        val scope = rememberCoroutineScope()
+
+        Button(onClick = {
+            scope.launch {
+                if (state.isSpeaking) {
+                    voiceToText.stopListening()
+                    painter.intValue = R.drawable.round_mic_24
+                    rotation.animateTo(0f, animationSpec = tween(500, easing = EaseInOut))
+                } else {
+                    voiceToText.startListening()
+                    painter.intValue = R.drawable.round_stop_24
+                    rotation.animateTo(360f, animationSpec = tween(500, easing = EaseInOut))
+                }
+            }
+        }) {
+            Icon(
+                painter = painterResource(id = painter.intValue),
+                contentDescription = "Record audio icon",
+                modifier = Modifier.rotate(rotation.value)
+            )
+        }
+
+        Spacer(modifier = Modifier.width(15.dp))
+
         AnimatedContent(targetState = state.isSpeaking) { isSpeaking ->
-            Text(text =
-                if (isSpeaking)
-                    "Konuşuluyor"
-                else
-                    state.spokenText.ifEmpty { "Mikrofona basın" }
+            Text(
+                text = if (isSpeaking) "Konuşuluyor" else state.spokenText.ifEmpty { "Mikrofona basın" },
+                style = MaterialTheme.typography.bodyLarge
             )
         }
     }
